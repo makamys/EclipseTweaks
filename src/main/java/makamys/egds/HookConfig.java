a57c01dbec8dc57af0756ed41657a20322c74b03
makamys <makamys@outlook.com>
2022-07-30 20:25:01 +0200

    Make it work

--- src/main/java/makamys/egds/HookConfig.java
+++ src/main/java/makamys/egds/HookConfig.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.eclipse.osgi.internal.hookregistry.ClassLoaderHook;
 import org.eclipse.osgi.internal.hookregistry.HookConfigurator;
@@ -11,6 +14,12 @@ import org.eclipse.osgi.internal.hookregistry.HookRegistry;
 import org.eclipse.osgi.internal.loader.classpath.ClasspathEntry;
 import org.eclipse.osgi.internal.loader.classpath.ClasspathManager;
 import org.eclipse.osgi.storage.bundlefile.BundleEntry;
+import org.gradle.tooling.GradleConnector;
+import org.gradle.tooling.ProjectConnection;
+import org.gradle.tooling.model.idea.IdeaDependency;
+import org.gradle.tooling.model.idea.IdeaModule;
+import org.gradle.tooling.model.idea.IdeaProject;
+import org.gradle.tooling.model.idea.IdeaSingleEntryLibraryDependency;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
@@ -104,13 +113,78 @@ public class HookConfig implements HookConfigurator {
     }
     
     public static String[] redirectGetClasspath(Object conf) {
+        String[] cp = new String[] {};
         try {
-            String[] result = (String[])conf.getClass().getMethod("getClassPath").invoke(conf);
-            return result;
+            cp = (String[])conf.getClass().getMethod("getClassPath").invoke(conf);
         } catch (Exception e) {
             log("Failed to call getClassPath: " + e.getMessage());
         }
-        return new String[] {};
+        
+        try {
+            List<String> providedDeps = getProvidedDeps(conf, cp).stream().map(p -> toCanonicalPath(p)).collect(Collectors.toList());
+            String[] goodCP = Arrays.stream(cp).filter(p -> !providedDeps.contains(toCanonicalPath(p))).toArray(String[]::new);
+            log("Original CP: " + Arrays.toString(cp));
+            log("Modified CP: " + Arrays.toString(goodCP));
+            cp = goodCP;
+        } catch(Exception e) {
+            log("Failed to modify class path: " + e.getMessage());
+        }
+        
+        return cp;
+    }
+    
+    private static String toCanonicalPath(String path) {
+        try {
+            return new File(path).getCanonicalPath();
+        } catch (IOException e) {
+            log("Failed to canonicalize " + path + ": " + e.getMessage());
+        }
+        return "";
+    }
+    
+    private static List<String> getProvidedDeps(Object conf, String[] cp) {
+        File gradleProjectDir = guessGradleProjectDir(conf, cp);
+        
+        List<String> files = new ArrayList<>();
+        
+        if(gradleProjectDir != null) {    
+            ProjectConnection connection = GradleConnector.newConnector().forProjectDirectory(gradleProjectDir).connect();
+            IdeaProject be = connection.model(IdeaProject.class).setJavaHome(new File(getJavaHome())).get();
+            for(IdeaModule module : be.getModules()) {
+                for(IdeaDependency dep : module.getDependencies()) {
+                    if(dep instanceof IdeaSingleEntryLibraryDependency) {
+                        IdeaSingleEntryLibraryDependency ldep = (IdeaSingleEntryLibraryDependency)dep;
+                        if(ldep.getScope().getScope().toLowerCase().equals("provided")) {
+                            files.add(ldep.getFile().getPath());
+                        }
+                    }
+                }
+            }
+        } else {
+            log("Failed to guess Gradle project dir. cp: " + Arrays.toString(cp));
+        }
+        
+        log("Provided dependencies: " + files);
+        
+        return files;
+    }
+    
+    private static String getJavaHome() {
+        // TODO
+        return System.getProperty("edgs.javaHome");
+    }
+    
+    private static File guessGradleProjectDir(Object conf, String[] cp) {
+        for(String string : cp) {
+            File f = new File(string);
+            while(f != null && !(f.isDirectory() && (f.getName().equals("bin") || f.getName().equals("build")))) {
+                f = f.getParentFile();
+            }
+            if(f != null) {
+                return f.getParentFile();
+            }
+        }
+        return null;
     }
 
 }
