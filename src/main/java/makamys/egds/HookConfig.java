a0b401a0e477e35b13a06fa37382f0bad29d4f73
makamys <makamys@outlook.com>
2022-07-30 18:02:47 +0200

    Add hook to beginning of StandardVMDebugger#getCommandLine

--- src/main/java/makamys/egds/HookConfig.java
+++ src/main/java/makamys/egds/HookConfig.java
@@ -3,7 +3,7 @@ package makamys.egds;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
+import java.util.ArrayList;
 
 import org.eclipse.osgi.internal.hookregistry.ClassLoaderHook;
 import org.eclipse.osgi.internal.hookregistry.HookConfigurator;
@@ -11,40 +11,89 @@ import org.eclipse.osgi.internal.hookregistry.HookRegistry;
 import org.eclipse.osgi.internal.loader.classpath.ClasspathEntry;
 import org.eclipse.osgi.internal.loader.classpath.ClasspathManager;
 import org.eclipse.osgi.storage.bundlefile.BundleEntry;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
 
 public class HookConfig implements HookConfigurator {
-
-    private static Writer out;
     
     @Override
     public void addHooks(HookRegistry hookRegistry) {
-        try {
-            out = new FileWriter(new File(System.getProperty("java.io.tmpdir"), "hook-config-test.log"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
         log("addHooks");
         hookRegistry.addClassLoaderHook(new MyClassLoaderHook());
     }
     
     public static void log(String msg) {
-        if(out != null) {
-            try {
-                out.write(msg + "\n");
-                out.flush();
-            } catch (IOException e) {
-                
+        try {
+            FileWriter out = new FileWriter(new File(System.getProperty("java.io.tmpdir"), "hook-config-test.log"), true);
+            out.write(msg + "\n");
+            out.flush();
+        } catch (IOException e) {
+            
+        }
+    }
+    
+    // XXX bad
+    private static String getJarPath() {
+        for(String ext : System.getProperty("osgi.framework.extensions").split(",")) {
+            if(ext.contains("EclipseGradleDependencyScope") && ext.startsWith("reference:file:")) {
+                return ext.substring("reference:file:".length());
             }
         }
+        return null;
     }
     
     public static class MyClassLoaderHook extends ClassLoaderHook {
         @Override
         public byte[] processClass(String name, byte[] classbytes, ClasspathEntry classpathEntry, BundleEntry entry,
                 ClasspathManager manager) {
-            return super.processClass(name, classbytes, classpathEntry, entry, manager);
+            if(name.equals("org.eclipse.jdt.internal.launching.StandardVMDebugger")) {
+                return transform(name, classbytes);
+            }
+            return null;
         }
+
+        private byte[] transform(String name, byte[] bytes) {
+            try {
+                ClassNode classNode = new ClassNode();
+                ClassReader classReader = new ClassReader(bytes);
+                classReader.accept(classNode, 0);
+                for(MethodNode m : classNode.methods) {
+                    String className = classNode.name;
+                    String methodName = m.name;
+                    String methodDesc = m.desc;
+                    if(methodName.equals("getCommandLine")) {
+                        log("Patching " + className + "#" + methodName + methodDesc);
+                        
+                        m.instructions.insert(new MethodInsnNode(Opcodes.INVOKESTATIC, "makamys/egds/HookConfig", "hook", "()V"));
+                    }
+                }
+                
+                ClassWriter writer = new ClassWriter(0);
+                classNode.accept(writer);
+                return writer.toByteArray();
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+            return bytes;
+        }
+        
+        public boolean addClassPathEntry(ArrayList<ClasspathEntry> cpEntries, String cp, ClasspathManager hostmanager, org.eclipse.osgi.storage.BundleInfo.Generation sourceGeneration) {
+            hostmanager.addClassPathEntry(cpEntries, "external:" + getJarPath(), hostmanager, sourceGeneration);
+            return true;
+        }
+    }
+    
+    
+    public static void hook() {
+        log("Hook called");
+    }
+    
+    public static void callHook() {
+        hook();
     }
 
 }
