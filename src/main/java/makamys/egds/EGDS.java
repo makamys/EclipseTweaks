b27b32fff00b5e0bf36af3efc46596c3a9287ac3
makamys <makamys@outlook.com>
2022-07-30 11:35:53 +0200

    Rename project to EclipseGradleDependencyScope

--- src/main/java/makamys/egds/EGDS.java
+++ src/main/java/makamys/egds/EGDS.java
@@ -1,8 +1,8 @@
-package makamys.fgech;
+package makamys.egds;
 
 import java.lang.instrument.Instrumentation;
 
-public class FGECH {
+public class EGDS {
 
     public static void premain(String agentArgs, Instrumentation inst) {
         System.out.println("hello premain");
